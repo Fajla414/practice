5628f7c525c968f61c26a2e81fd33dfc56b4a7ca
Fajla Rabby <blfajlarabby414@gmail.com>
2024-08-02 16:35:45 +0600

    timeout.js about setTimeout and setInterval

---
+++ timeout.js
@@ -0,0 +1,15 @@
+function doSomething() {
+    console.log(3333);
+
+}
+console.log(2222);
+// doSomething();
+// setTimeout(() => {
+//     console.log('lazy and waiting');
+// }, 4000);
+setInterval(()=>{
+    console.log('doing it again');
+}, 3000)
+console.log(4444);
+
+
